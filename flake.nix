d1e9d7674a1d0adb0fc5a3c5f056d2c107b1b2b6
Ranadeep Biswas <mail@rnbguy.at>
2024-11-17 10:42:29 +0100

    nix fmt

--- flake.nix
+++ flake.nix
@@ -10,46 +10,51 @@
 
   outputs =
     inputs:
-    inputs.flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
-      system:
-      let
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            inputs.rust-overlay.overlays.default
-            inputs.ethereumNix.overlays.default
-          ];
-        };
-      in
-      {
-        packages = with pkgs; {
-          inherit
-            just
-            cargo-nextest
-            reth
-            foundry-bin
-            ;
-          default = pkgs.symlinkJoin {
-            name = "combined-default";
-            paths = [
+    inputs.flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+      ]
+      (
+        system:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+              inputs.ethereumNix.overlays.default
+            ];
+          };
+        in
+        {
+          packages = with pkgs; {
+            inherit
+              just
+              cargo-nextest
+              reth
+              foundry-bin
+              ;
+            default = pkgs.symlinkJoin {
+              name = "combined-default";
+              paths = [
+                just
+                cargo-nextest
+                reth
+                foundry-bin
+              ];
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
               just
               cargo-nextest
               reth
               foundry-bin
             ];
           };
-        };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            just
-            cargo-nextest
-            reth
-            foundry-bin
-          ];
-        };
-
-        formatter = pkgs.nixfmt-rfc-style;
-      }
-    );
+          formatter = pkgs.nixfmt-rfc-style;
+        }
+      );
 }
