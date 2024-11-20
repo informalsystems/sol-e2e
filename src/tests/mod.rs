42e9b8d6ec9e5644ba360b818c261ec9288226da
Ranadeep Biswas <mail@rnbguy.at>
2024-11-20 01:25:58 +0100

    health check for 3mins

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -18,9 +18,24 @@ async fn test_eth_e2e(
     #[case] scenario: impl Scenario,
 ) -> TestResult {
     network.start().await?;
-    while network.health_check().await.is_err() {
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+    let timeout = tokio::time::sleep(tokio::time::Duration::from_secs(180)); // 30 minutes
+    tokio::pin!(timeout);
+
+    loop {
+        tokio::select! {
+            _ = &mut timeout => {
+                network.stop().await?;
+                return Err("Network health check timed out after 3 minutes".into());
+            }
+            _ = tokio::time::sleep(tokio::time::Duration::from_secs(1)) => {
+                if network.health_check().await.is_ok() {
+                    break;
+                }
+            }
+        }
     }
+
     let config = network.network_config();
     let resp = scenario.run(config).await;
     network.stop().await?;
