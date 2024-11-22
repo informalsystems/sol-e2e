a33cbce90c566a98875a5b07de2c54814d0f4c3f
Ranadeep Biswas <mail@rnbguy.at>
2024-11-22 15:31:47 +0100

    test using union beacon client

--- src/tests/scenario/finality.rs
+++ src/tests/scenario/finality.rs
@@ -1,9 +1,7 @@
+use anyhow::Context;
 use testresult::TestResult;
 
 use crate::tests::network::EthereumConfig;
-use helios_consensus_core::consensus_spec::MinimalConsensusSpec;
-use helios_ethereum::rpc::http_rpc::HttpRpc;
-use helios_ethereum::rpc::ConsensusRpc;
 
 use super::Scenario;
 
@@ -13,34 +11,50 @@ impl Scenario for Finality {
     async fn run(&self, config: EthereumConfig) -> TestResult {
         let EthereumConfig { cl_socket, .. } = config;
 
-        let helios_client = <HttpRpc as ConsensusRpc<MinimalConsensusSpec>>::new(&format!(
+        let beacon_client = beacon_api::client::BeaconApiClient::new(format!(
             "http://{}",
-            cl_socket.unwrap()
-        ));
+            cl_socket.context("no cl_socket")?
+        ))
+        .await?;
 
-        while <HttpRpc as ConsensusRpc<MinimalConsensusSpec>>::get_finality_update(&helios_client)
-            .await
-            .is_err()
-        {
-            tokio::time::sleep(core::time::Duration::from_secs(1)).await;
-        }
+        let spec = beacon_client.spec().await?;
+        println!("{}", serde_json::to_string_pretty(&spec)?);
 
-        let finality_update =
-            <HttpRpc as ConsensusRpc<MinimalConsensusSpec>>::get_finality_update(&helios_client)
-                .await?;
+        let seconds_per_sync_committee_period = spec.data.seconds_per_slot
+            * spec.data.slots_per_epoch
+            * spec.data.epochs_per_sync_committee_period;
 
-        println!("{:#?}", finality_update);
+        println!(
+            "wait for sync committee period: {} seconds",
+            seconds_per_sync_committee_period
+        );
 
-        let optimistic_update =
-            <HttpRpc as ConsensusRpc<MinimalConsensusSpec>>::get_optimistic_update(&helios_client)
-                .await?;
+        tokio::time::sleep(tokio::time::Duration::from_secs(
+            seconds_per_sync_committee_period,
+        ))
+        .await;
 
-        println!("{:#?}", optimistic_update);
+        let finality_update = beacon_client.finality_update().await?;
+        println!("{}", serde_json::to_string_pretty(&finality_update)?);
 
-        let block =
-            <HttpRpc as ConsensusRpc<MinimalConsensusSpec>>::get_block(&helios_client, 1).await?;
+        let finalized_slot = finality_update.data.finalized_header.beacon.slot;
 
-        println!("{:#?}", block);
+        let finalized_header = beacon_client.header(finalized_slot.into()).await?;
+        println!("{}", serde_json::to_string_pretty(&finalized_header)?);
+
+        let finalized_root = finalized_header.data.root;
+
+        let finalized_block = beacon_client.block(finalized_slot.into()).await?;
+        println!("{}", serde_json::to_string_pretty(&finalized_block)?);
+
+        let bootstrap = beacon_client.bootstrap(finalized_root).await?;
+        println!("{}", serde_json::to_string_pretty(&bootstrap)?);
+
+        let resp = beacon_client.genesis().await?;
+        println!("{}", serde_json::to_string_pretty(&resp)?);
+
+        let light_client_updates = beacon_client.light_client_updates(0, 1).await?;
+        println!("{}", serde_json::to_string_pretty(&light_client_updates)?);
 
         Ok(())
     }
